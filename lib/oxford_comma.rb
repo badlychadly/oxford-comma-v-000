caa7c4b00d803d919fe190d148676e6ea90219c8
Chad Montoya <chadmontoya21@yahoo.com>
2017-10-24 19:20:55 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,13 +1,23 @@
 require 'pry'
 
+# def oxford_comma(array)
+#   if array.length == 1
+#     array.join
+#   elsif array.length == 2
+#     array.join(" and ")
+#   else
+#     last = array.slice!(array.length-1)
+#     array.push("and #{last}")
+#     array.join(", ")
+#   end
+# end
+
+
 def oxford_comma(array)
-  if array.length == 1
-    array.join
-  elsif array.length == 2
-    array.join(" and ")
-  else
-    last = array.slice!(array.length-1)
-    array.push("and #{last}")
-    array.join(", ")
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif 2 < array.length
+    array[-1].insert(0, "and ")
   end
+  array.join(", ")    
 end
